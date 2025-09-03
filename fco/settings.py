2c425b4883d8d9110a03d62e28976d565cd7bcba
rofiareiv <kambing.rofi@gmail.com>
2025-09-03 08:23:44 +0700

    config django and pymysql

--- fco/settings.py
+++ fco/settings.py
@@ -72,10 +72,25 @@ WSGI_APPLICATION = 'fco.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fco',
+        'USER': 'root',
+        'PASSWORD': 'rahasia',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        },        
     }
 }
 
